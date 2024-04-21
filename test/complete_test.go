18b43383419ff4efb54638c5902773b4c9dee7d4
Michael Ullrich <michael.ullrich@nuvibit.com>
2024-04-21 12:19:32 +0200

    initial version

--- test/complete_test.go
+++ test/complete_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestExampleComplete(t *testing.T) {
@@ -19,10 +20,13 @@ func TestExampleComplete(t *testing.T) {
 	defer terraform.Destroy(t, terraformOptions)
 	terraform.InitAndApply(t, terraformOptions)
 
-	accountidOutput := terraform.Output(t, terraformOptions, "account_id")
-	t.Log(accountidOutput)
+	// Retrieve the 'test_success' outputs
+	testSuccess1Output := terraform.Output(t, terraformOptions, "test_success1")
+	testSuccess2Output := terraform.Output(t, terraformOptions, "test_success2")
+	t.Logf("Lambda testSuccess1Output: %s", testSuccess1Output)
+	t.Logf("Lambda testSuccess2Output: %s", testSuccess2Output)
 
-	inputOutput := terraform.Output(t, terraformOptions, "input")
-	t.Log(inputOutput)
-	// Do testing. I.E check if your ressources are deployed via AWS GO SDK
+	// Assert that 'test_success' equals "true"
+	assert.Equal(t, "true", testSuccess1Output, "The test_success1 output is not true")
+	assert.Equal(t, "true", testSuccess2Output, "The test_success2 output is not true")
 }
