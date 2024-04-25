fd82242aa547c9a62259c968453ca2ee865c2311
Michael Ullrich <michael.ullrich@nuvibit.com>
2024-04-25 15:57:51 +0200

    fx

--- reporting/main.tf
+++ reporting/main.tf
@@ -12,65 +12,51 @@ terraform {
     }
   }
 }
-
-
 data "aws_organizations_organization" "org" {}
 
 data "aws_organizations_organizational_units" "root_ous" {
   parent_id = data.aws_organizations_organization.org.roots[0].id
 }
 
+locals {
+  root_ous_paths = { for ou in data.aws_organizations_organizational_units.root_ous.children : ou.id => "/${ou.name}" }
+}
+
 data "aws_organizations_organizational_units" "level_1_ous" {
-  for_each = { for ou in data.aws_organizations_organizational_units.root_ous.children : "/root/${ou.name}" => ou.id }
+  for_each = { for ou in data.aws_organizations_organizational_units.root_ous.children : "${data.aws_organizations_organization.org.roots[0].id}-${ou.name}" => ou.id }
   parent_id = each.value
+}
+
+locals {
+  level_1_ous_paths = { for ou_id, ou in data.aws_organizations_organizational_units.level_1_ous : ou.id => "${local.root_ous_paths[ou.parent_id]}/${ou.name}" }
 }
 
 data "aws_organizations_organizational_units" "level_2_ous" {
-  for_each = { for ou in flatten([for parent in data.aws_organizations_organizational_units.level_1_ous : parent.children]) : "/root/${parent.name}/${ou.name}" => ou.id }
+  for_each = { for ou in flatten([for parent in data.aws_organizations_organizational_units.level_1_ous : [for child in parent.children : { name = child.name, id = child.id, parent_id = parent.id }]]) : "${ou.parent_id}-${ou.name}" => ou.id }
   parent_id = each.value
 }
 
+locals {
+  level_2_ous_paths = { for ou_id, ou in data.aws_organizations_organizational_units.level_2_ous : ou.id => "${local.level_1_ous_paths[ou.parent_id]}/${ou.name}" }
+}
+
 data "aws_organizations_organizational_units" "level_3_ous" {
-  for_each = { for ou in flatten([for parent in data.aws_organizations_organizational_units.level_2_ous : parent.children]) : ou.name => ou.id }
+  for_each = { for ou in flatten([for parent in data.aws_organizations_organizational_units.level_2_ous : [for child in parent.children : { name = child.name, id = child.id, parent_id = parent.id }]]) : "${ou.parent_id}-${ou.name}" => ou.id }
   parent_id = each.value
 }
 
 data "aws_organizations_organizational_units" "level_4_ous" {
-  for_each = { for ou in flatten([for parent in data.aws_organizations_organizational_units.level_3_ous : parent.children]) : ou.name => ou.id }
+  for_each = { for ou in flatten([for parent in data.aws_organizations_organizational_units.level_3_ous : [for child in parent.children : { name = child.name, id = child.id, parent_id = parent.id }]]) : "${ou.parent_id}-${ou.name}" => ou.id }
   parent_id = each.value
 }
 
 data "aws_organizations_organizational_units" "level_5_ous" {
-  for_each = { for ou in flatten([for parent in data.aws_organizations_organizational_units.level_4_ous : parent.children]) : ou.name => ou.id }
+  for_each = { for ou in flatten([for parent in data.aws_organizations_organizational_units.level_4_ous : [for child in parent.children : { name = child.name, id = child.id, parent_id = parent.id }]]) : "${ou.parent_id}-${ou.name}" => ou.id }
   parent_id = each.value
 }
 
 
-/*
-data "aws_organizations_organizational_units" "level_2_ous" {
-  count     = length(data.aws_organizations_organizational_units.level_1_ous.children)
-  parent_id = data.aws_organizations_organizational_units.level_1_ous.children[count.index].id
-}
-/*
 
-data "aws_organizations_organizational_units" "level_3_ous" {
-  count     = length(data.aws_organizations_organizational_units.level_2_ous.children)
-  parent_id = data.aws_organizations_organizational_units.level_2_ous.children[count.index].id
-}
-
-
-data "aws_organizations_organizational_units" "level_4_ous" {
-  count     = length(data.aws_organizations_organizational_units.level_3_ous.children)
-  parent_id = data.aws_organizations_organizational_units.level_3_ous.children[count.index].id
-}
-
-
-data "aws_organizations_organizational_units" "level_5_ous" {
-  count     = length(data.aws_organizations_organizational_units.level_4_ous.children)
-  parent_id = data.aws_organizations_organizational_units.level_4_ous.children[count.index].id
-}
-
-*/
 output "root_ou" {
   value = {
     "/root" = data.aws_organizations_organization.org.roots[0].id
@@ -78,9 +64,9 @@ output "root_ou" {
 }
 
 output "level_1_ous" {
-  value = data.aws_organizations_organizational_units.level_1_ous
+  value = local.root_ous_paths
 }
 
 output "level_2_ous" {
-  value = data.aws_organizations_organizational_units.level_2_ous
+  value = local.level_2_ous_paths
 }
