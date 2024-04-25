92a9bbfe4440eba22e43bc1eb9e105f71119104d
Michael Ullrich <michael.ullrich@nuvibit.com>
2024-04-25 15:59:06 +0200

    fx

--- reporting/main.tf
+++ reporting/main.tf
@@ -12,6 +12,7 @@ terraform {
     }
   }
 }
+
 data "aws_organizations_organization" "org" {}
 
 data "aws_organizations_organizational_units" "root_ous" {
@@ -19,25 +20,28 @@ data "aws_organizations_organizational_units" "root_ous" {
 }
 
 locals {
+  # Storing root OUs with their names
   root_ous_paths = { for ou in data.aws_organizations_organizational_units.root_ous.children : ou.id => "/${ou.name}" }
 }
 
 data "aws_organizations_organizational_units" "level_1_ous" {
-  for_each = { for ou in data.aws_organizations_organizational_units.root_ous.children : "${data.aws_organizations_organization.org.roots[0].id}-${ou.name}" => ou.id }
-  parent_id = each.value
+  for_each = { for ou in data.aws_organizations_organizational_units.root_ous.children : ou.id => ou }
+  parent_id = each.value.id
 }
 
 locals {
-  level_1_ous_paths = { for ou_id, ou in data.aws_organizations_organizational_units.level_1_ous : ou.id => "${local.root_ous_paths[ou.parent_id]}/${ou.name}" }
+  # Creating paths for level 1 OUs by using parent information
+  level_1_ous_paths = { for ou_id, ou in data.aws_organizations_organizational_units.level_1_ous : ou_id => "${local.root_ous_paths[each.value.parent_id]}/${ou.name}" }
 }
 
+# Assuming data and locals for level 2 and beyond follow a similar structure:
 data "aws_organizations_organizational_units" "level_2_ous" {
-  for_each = { for ou in flatten([for parent in data.aws_organizations_organizational_units.level_1_ous : [for child in parent.children : { name = child.name, id = child.id, parent_id = parent.id }]]) : "${ou.parent_id}-${ou.name}" => ou.id }
-  parent_id = each.value
+  for_each = { for ou in flatten([for parent in data.aws_organizations_organizational_units.level_1_ous : [for child in parent.children : { id = child.id, name = child.name, parent_id = parent.id }]]) : ou.id => ou }
+  parent_id = each.value.id
 }
 
 locals {
-  level_2_ous_paths = { for ou_id, ou in data.aws_organizations_organizational_units.level_2_ous : ou.id => "${local.level_1_ous_paths[ou.parent_id]}/${ou.name}" }
+  level_2_ous_paths = { for ou_id, ou in data.aws_organizations_organizational_units.level_2_ous : ou_id => "${local.level_1_ous_paths[ou.parent_id]}/${ou.name}" }
 }
 
 data "aws_organizations_organizational_units" "level_3_ous" {
